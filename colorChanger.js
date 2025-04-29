67bae23d5758ee05c85e733f000057f03237b937
ColonShansika <shaniska84@gmail.com>
2025-04-29 12:12:55 -0400

    Update colorChanger.js

--- colorChanger.js
+++ colorChanger.js
@@ -1,1 +1,9 @@
+let button = document.querySelector("_________");
+let box = document.querySelector("_________");
+
+button.addEventListener("click", function() {
+  // code to change the color goes here
+});
+
+box.style.backgroundColor = "_________";
 
